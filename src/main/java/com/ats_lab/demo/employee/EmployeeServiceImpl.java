aba2cd8342c83b3e9a3bfcc1170b7de46fae1a62
Jongkonrat <jongkonrat.p@aware.co.th>
2024-06-27 16:12:31 +0700

    example logging

--- src/main/java/com/ats_lab/demo/employee/EmployeeServiceImpl.java
+++ src/main/java/com/ats_lab/demo/employee/EmployeeServiceImpl.java
@@ -5,6 +5,7 @@ import com.ats_lab.demo.common.repository.EmployeeRepository;
 import com.ats_lab.demo.employee.dto.CreateEmployeeDTO;
 import com.ats_lab.demo.employee.dto.UpdateEmployeeDTO;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ import java.util.Optional;
 
 @Service
 @Setter(onMethod_ = @Autowired)
+@Slf4j
 public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository ;
@@ -22,6 +24,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public EmployeeEntity createEmployee(CreateEmployeeDTO createEmployeeDTO) {
+        log.info("EmployeeServiceImpl.createEmployee");
         EmployeeEntity employee = new EmployeeEntity();
         employee.setEmpCode(createEmployeeDTO.getEmpCode());
         employee.setFirstName(createEmployeeDTO.getFirstName());
@@ -40,27 +43,32 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setUpdatedDate(new Date());
         employee.setUpdatedBy("admin");
 
+        log.info("EmployeeServiceImpl.createEmployee save : employeeEntity :: {}", employee);
         return employeeRepository.save(employee);
     }
 
     @Override
     public List<EmployeeEntity> getAllEmployee() {
+        log.info("EmployeeServiceImpl.getAllEmployee");
         return employeeRepository.findAll();
     }
 
     @Override
     public EmployeeEntity getEmployeeByEmpId(Integer empId) {
+        log.info("EmployeeServiceImpl.getEmployee with empId : {}", empId);
         return employeeRepository.findById(empId).orElseThrow(() -> new RuntimeException("Employee not found with id: " + empId));
     }
 
     @Override
     public Optional<EmployeeEntity> getEmployeeByEmpCode(String empCode) {
+        log.info("EmployeeServiceImpl.getEmployee with empCode : {}", empCode);
         Optional<EmployeeEntity> employee = Optional.ofNullable(employeeRepository.findByEmpCode(empCode));
         return Optional.ofNullable(employee.orElseThrow(() -> new RuntimeException("Employee not found with empCode" + empCode)));
     }
 
     @Override
     public EmployeeEntity updateEmployee(Integer empId, UpdateEmployeeDTO updateEmployeeDTO) {
+        log.info("EmployeeServiceImpl.updateEmployee with empId : {}", empId);
         EmployeeEntity existingEmployee = employeeRepository.findById(empId)
                 .orElseThrow(() -> new RuntimeException("Employee not found with id: " + empId));
 
@@ -73,11 +81,13 @@ public class EmployeeServiceImpl implements EmployeeService {
         existingEmployee.setUpdatedDate(new Date());
         existingEmployee.setUpdatedBy("admin");
 
+        log.info("EmployeeServiceImpl.updateEmployee Entity : {}", existingEmployee);
         return employeeRepository.save(existingEmployee);
     }
 
     @Override
     public void deleteEmployee(Integer empId) {
+        log.info("EmployeeServiceImpl.deleteEmployee with empId : {}", empId);
         EmployeeEntity existingEmployee = employeeRepository.findById(empId)
                 .orElseThrow(() -> new RuntimeException("Employee not found with id: " + empId));
         employeeRepository.delete(existingEmployee);
